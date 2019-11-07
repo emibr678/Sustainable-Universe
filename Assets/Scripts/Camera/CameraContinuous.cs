6a357504504867d4080ca908d437f36302565aa8
Kent-beep <56262701+Kent-beep@users.noreply.github.com>
2019-11-07 15:23:17 +0100

    camera done

--- Assets/Scripts/Camera/CameraContinuous.cs
+++ Assets/Scripts/Camera/CameraContinuous.cs
@@ -4,22 +4,69 @@ using UnityEngine;
 
 public class CameraContinuous : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 30;
+
+    private float outerLeft;
+    private float outerRight;
+    private float outerDown;
+    private float outerUp;
+    
     void Start()
     {
-        Debug.Log("Screen Width : " + Screen.width);
-        Debug.Log("Screen hiegh : " + Screen.height);
+        outerLeft = GetComponent<CameraDrag>().outerLeft;
+        outerRight = GetComponent<CameraDrag>().outerRight;
+        outerDown = GetComponent<CameraDrag>().outerDown;
+        outerUp = GetComponent<CameraDrag>().outerUp;      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.mousePosition.x > Screen.width -10)
+       
+        if (!Input.GetMouseButtonDown(1))
         {
-            Debug.Log("TEST");
+            if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= 0
+            && Input.mousePosition.y <= Screen.height && Input.mousePosition.y >= 0)
+            {
+
+                if (Input.mousePosition.x >= Screen.width - 15) //move camera right
+                {
+                    if (this.transform.position.x < outerRight)
+                    {
+                        Vector3 movex = new Vector3(0.1f * speed, 0, 0);
+                        transform.Translate(movex * Time.deltaTime, Space.World);
+                    }
+                }
+
+                if (Input.mousePosition.x <= 15)
+                {
+                    if (this.transform.position.x > outerLeft) //move camera left
+                    {
+                        Vector3 movex = new Vector3(-0.1f * speed, 0, 0);
+                        transform.Translate(movex * Time.deltaTime, Space.World);
+                    }
+                }
+
+                if (Input.mousePosition.y <= 15)
+                {
+                    if (this.transform.position.y > outerDown) //move camera down
+                    {
+                        Vector3 movey = new Vector3(0, -0.1f * speed, 0);
+                        transform.Translate(movey * Time.deltaTime, Space.World);
+                    }
+                }
+
+                if (Input.mousePosition.y >= Screen.height - 15) //move camera up
+                {
+                    if (this.transform.position.y < outerUp)
+                    {
+                        Vector3 movey = new Vector3(0, 0.1f * speed, 0);
+                        transform.Translate(movey * Time.deltaTime, Space.World);
+                    }
+                }
+
+            }
         }
-
-      
-
+        
     }
 }
--- Assets/Scripts/Camera/CameraDrag.cs
+++ Assets/Scripts/Camera/CameraDrag.cs
@@ -4,85 +4,65 @@ using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
 {
-    public float dragSpeed = 20;
-    private Vector3 dragOrigin;
-
-    public bool cameraDragging = true;
-
+    public float dragSpeed = 30;
+   
     public float outerLeft = -10f;
     public float outerRight = 10f;
     public float outerUp = 10f;
     public float outerDown = -10f;
 
+    private Vector3 dragOrigin;
 
     void Update()
     {
-        /*
-        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        
-        float left = Screen.width * 0.2f;
-        float right = Screen.width - (Screen.width * 0.2f);
-
-        if (mousePosition.x < left)
+       
+        if (Input.GetMouseButtonDown(1))
         {
-            cameraDragging = true;
+            dragOrigin = Input.mousePosition;
+            return;
         }
-        else if (mousePosition.x > right)
+
+        if (!Input.GetMouseButton(1)) return;
+        GetComponent<CameraContinuous>().enabled = false;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+
+        // moves in x-axis
+        Vector3 movex = new Vector3(pos.x * dragSpeed, 0, 0);
+        if (movex.x > 0f)
         {
-            cameraDragging = true;
-        }*/
-
-        if (cameraDragging)
-        {
-
-            if (Input.GetMouseButtonDown(1))
+            if (this.transform.position.x < outerRight)
             {
-                dragOrigin = Input.mousePosition;
-                return;
-            }
-
-            if (!Input.GetMouseButton(1)) return;
-
-            Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
-           
-            // moves in x-axis
-            Vector3 movex = new Vector3(pos.x * dragSpeed, 0, 0); 
-            if (movex.x > 0f)
-            {
-                if (this.transform.position.x < outerRight)
-                {
-                    transform.Translate(movex * Time.deltaTime, Space.World);
-                }
-            }
-            else
-            {
-                if (this.transform.position.x > outerLeft)
-                {
-                    transform.Translate(movex * Time.deltaTime, Space.World);
-                }
-            }
-
-            // moves in y-axis   
-            Vector3 movey = new Vector3(0, pos.y * dragSpeed, 0);
-            if (movey.y > 0f)
-            {
-                if (this.transform.position.y < outerUp)
-                {
-                    transform.Translate(movey * Time.deltaTime, Space.World);
-                }
-            }
-            else
-            {
-                if (this.transform.position.y > outerDown)
-                {
-                    transform.Translate(movey * Time.deltaTime, Space.World);
-                }
+                transform.Translate(movex * Time.deltaTime, Space.World);
             }
         }
+        else
+        {
+            if (this.transform.position.x > outerLeft)
+            {
+                transform.Translate(movex * Time.deltaTime, Space.World);
+            }
+        }
+
+        // moves in y-axis   
+        Vector3 movey = new Vector3(0, pos.y * dragSpeed, 0);
+        if (movey.y > 0f)
+        {
+            if (this.transform.position.y < outerUp)
+            {
+                transform.Translate(movey * Time.deltaTime, Space.World);
+            }
+        }
+        else
+        {
+            if (this.transform.position.y > outerDown)
+            {
+                transform.Translate(movey * Time.deltaTime, Space.World);
+            }
+        }
+        GetComponent<CameraContinuous>().enabled = true;
+
     }
-
-    
 }
 
 
-   
