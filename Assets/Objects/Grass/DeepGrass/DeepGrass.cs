38104b55ed01ff3a0decb77e182855f9be94c324
emibr678 <emibr678@student.liu.se>
2019-11-19 22:56:50 +0100

    Emil
    Rocks with Collider
    Sheeps move toward Deep Grass / NOT Completey done
    Deep Grass will grow if sheep does not eat it
    *Extra press + or - to speed up or slow down the game speed.

---
+++ Assets/Objects/Grass/DeepGrass/DeepGrass.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DeepGrass : MonoBehaviour
+{
+    Tilemap tilemap;
+    Tilemap ground_tilemap;
+    Vector3Int grid_position;
+    
+    public int health;
+    
+   /* public int TakeHealth(int want)
+    {
+        if(health > want)
+        {
+            health -= want;
+        }
+        else
+        {
+            want = health;
+            health = 0;
+        }
+        
+        return want;
+    } */
+    
+    void Start()
+    {
+        tilemap = GameObject.Find("World").GetComponent<Tilemap>();
+        ground_tilemap = GameObject.Find("Ground").GetComponent<Tilemap>();
+        grid_position = tilemap.WorldToCell(transform.position);
+    }
+    
+    void Update()
+    {
+        if(health <= 0)
+        {
+            tilemap.SetTile(grid_position, null);
+        }
+        else
+        {
+            Vector3Int[] neighbors_directions = new Vector3Int[]
+            {
+                new Vector3Int(1, 0, 0),
+                new Vector3Int(-1, 0, 0),
+                new Vector3Int(0, 1, 0),
+                new Vector3Int(0, -1, 0)
+            };
+            
+            foreach(Vector3Int dir in neighbors_directions)
+            {
+                Vector3Int neighbor_position = grid_position + dir;
+                Tile tile = (Tile)tilemap.GetTile(neighbor_position);
+                Tile ground_tile = (Tile)ground_tilemap.GetTile(neighbor_position);
+                
+                if(tile == null && ground_tile != null)
+                {
+                    //Assume grass for now.
+                    if(Random.Range(0.0f, 100.0f) < 0.01f)
+                    {
+                        tilemap.SetTile(neighbor_position, tilemap.GetTile(grid_position));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Objects/Sheep/SheepMovement.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public bool isWalking;
+    public float walkTime;
+    public float waitTime;
+    private float walkCounter;
+    private float waitCounter;
+    private Rigidbody2D rb;
+    public Animator anim;
+
+    private int WalkDirection;
+
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        walkCounter = walkTime;
+        anim = GetComponent<Animator> ();
+		
+        ChooseDirection();
+    }
+
+    void Update() {
+        if(isWalking) {
+            walkCounter -= Time.deltaTime;
+            
+            switch(WalkDirection) {
+                case 0:
+                    rb.velocity = new Vector2(0, 2);
+                    //anim.SetBool("NW", true);
+                    //Debug.Log("0");
+                    break;
+                
+                case 1:
+                    rb.velocity = new Vector2(1, 0);
+                  //   anim.SetBool("NE", true);
+                   // Debug.Log("1");
+                    break;
+               
+                case 2:
+                    rb.velocity = new Vector2(0, -2);
+                 //    anim.SetBool("SW", true);
+                   // Debug.Log("2");
+                    break;
+                
+                case 3:
+                    rb.velocity = new Vector2(-1, 0);
+                //     anim.SetBool("SE", true);
+                  //  Debug.Log("3");
+                    break;
+
+                /*case 4:
+                    rb.velocity = new Vector2(0, 1);
+                   // Debug.Log("4");
+                    break;
+                case 5:
+                    rb.velocity = new Vector2(0, -1);
+                   // Debug.Log("5");
+                    break;
+                case 6:
+                    rb.velocity = new Vector2(1, -1);
+                  //  Debug.Log("6");
+                    break;
+                case 7:
+                    rb.velocity = new Vector2(1, 1);
+                   // Debug.Log("7");
+                    break; */
+            }
+
+            if(walkCounter < 0) {
+                isWalking = false;
+                waitCounter = waitTime;
+            }
+        } 
+        
+        else {
+            waitCounter -= Time.deltaTime;
+            rb.velocity = Vector2.zero;
+            if(waitCounter < 0){
+                ChooseDirection(); 
+            }
+
+        }
+    }
+
+    public void ChooseDirection() {
+        WalkDirection =  Random.Range(0, 8);
+       // Debug.Log(WalkDirection);
+        isWalking = true;
+        walkCounter = walkTime;
+    }
+    /*public float movementSpeed = 0.1f;
+    IsometricCharacterRenderer isoRenderer;
+
+    Rigidbody2D rbody;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 currentPos = rbody.position;
+        //float horizontalInput = 0;
+       // float verticalInput = -4;
+       //Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        Vector2 pos = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+        pos = Vector2.ClampMagnitude(pos, 1);
+        Vector2 movement = pos * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+       
+        isoRenderer.SetDirection(movement);
+        rbody.MovePosition(newPos);
+    }
+    */
+}
